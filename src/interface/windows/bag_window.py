e3ee300664f3974de9126c6e4cf2b63224d03ac0
huayu1115 <vivian989370@gmail.com>
2025-11-23 01:34:07 +0800

    bag add img

--- src/interface/windows/bag_window.py
+++ src/interface/windows/bag_window.py
@@ -2,6 +2,7 @@
 import pygame as pg
 from src.interface.windows.window import Window 
 from src.core import GameManager
+from src.utils import load_img, Logger
 
 class BagWindow(Window):
     def __init__(self, game_manager: GameManager, font_title: pg.font.Font, font_item: pg.font.Font):
@@ -10,6 +11,7 @@ class BagWindow(Window):
         
         self.font_title = font_title
         self.font_item = font_item
+
 
     def draw(self, screen: pg.Surface):
         self.draw_background(screen)
@@ -25,29 +27,53 @@ class BagWindow(Window):
         screen.blit(title_backpack, title_rect)
 
         # 物品列表
-        item_title = self.font_title.render("Items", True, (0, 0, 0))
-        screen.blit(item_title, (self.rect.x + 50, self.rect.y + 100))
+        item_title = self.font_item.render("Items", True, (0, 0, 0))
+        screen.blit(item_title, (self.rect.x + 50, self.rect.y + 90))
 
-        current_y = self.rect.y + 150 # 列表起始 Y 座標
+        current_y = self.rect.y + 120 # 列表起始 Y 座標
         for item in items:
             item_name = item.get("name", "Unknown Item")
-            item_quantity = item.get("quantity", 1)
+            item_quantity = item.get("count", item.get("quantity", 1))
             
             text = f"{item_name} x{item_quantity}"
             item_text = self.font_item.render(text, True, (50, 50, 50))
             screen.blit(item_text, (self.rect.x + 60, current_y))
-            current_y += 35 # 換行
+            current_y += 45 # 換行
 
         # 怪物列表
-        monster_title = self.font_title.render("Monsters", True, (0, 0, 0))
-        screen.blit(monster_title, (self.rect.centerx + 50, self.rect.y + 100))
+        monster_title = self.font_item.render("Monsters", True, (0, 0, 0))
+        screen.blit(monster_title, (self.rect.centerx + 50, self.rect.y + 90))
         
-        current_y = self.rect.y + 150 # 重置 Y 座標
+        current_y = self.rect.y + 120 # 重置 Y 座標
         for monster in monsters:
             monster_name = monster.get("name", "Unknown Monster")
             monster_level = monster.get("level", 1)
+            monster_hp = monster.get("hp", 0)
+            monster_max_hp = monster.get("max_hp", 100)
+            sprite_path = monster.get("sprite_path", None)
 
-            text = f"Lv.{monster_level} {monster_name}"
-            monster_text = self.font_item.render(text, True, (50, 50, 50))
-            screen.blit(monster_text, (self.rect.centerx + 60, current_y))
-            current_y += 35 # 換行
+            # 載入並縮放圖片
+            icon_size = 35
+            icon_x = self.rect.centerx + 20
+
+            if sprite_path:
+                try: 
+                    image = load_img(sprite_path)
+                    image = pg.transform.scale(image, (icon_size, icon_size))
+                    screen.blit(image, (icon_x, current_y))
+                except Exception as e:
+                    pg.draw.rect(screen, (150, 150, 150), (icon_x, current_y, icon_size, icon_size))
+
+            # 怪獸文字資訊
+            text_x = icon_x + icon_size + 10
+
+            name_text = f"Lv.{monster_level} {monster_name}"
+            name_surface = self.font_item.render(name_text, True, (50, 50, 50))
+            screen.blit(name_surface, (text_x, current_y))
+
+            hp_text = f"HP: {monster_hp}/{monster_max_hp}"
+            hp_color = (200, 50, 50) if monster_hp < monster_max_hp * 0.2 else (50, 50, 50)
+            hp_surface = self.font_item.render(hp_text, True, hp_color)
+            screen.blit(hp_surface, (text_x, current_y + 20))
+
+            current_y += 45
